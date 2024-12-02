603a781b8d4bef5f6cba44fda024f460720476d9
Spacetech <824323+Spacetech@users.noreply.github.com>
2024-12-02 06:02:38 -0800

    Update for 2.14.0-beta.dev.20241202.1

--- src/ChallengeServer.ts
+++ src/ChallengeServer.ts
@@ -1,19 +1,19 @@
 import { EventBus } from "@wayward/game/event/EventBuses";
 import { EventHandler } from "@wayward/game/event/EventManager";
 import { PlayerState } from "@wayward/game/game/entity/player/IPlayer";
-import Player from "@wayward/game/game/entity/player/Player";
-import PlayerManager from "@wayward/game/game/entity/player/PlayerManager";
-import { Game } from "@wayward/game/game/Game";
+import type Player from "@wayward/game/game/entity/player/Player";
+import type PlayerManager from "@wayward/game/game/entity/player/PlayerManager";
+import type { Game } from "@wayward/game/game/Game";
 import { PauseSource } from "@wayward/game/game/IGame";
 import DedicatedServerManager from "@wayward/game/game/meta/DedicatedServerManager";
 import { GameMode } from "@wayward/game/game/options/IGameOptions";
-import Dictionary from "@wayward/game/language/Dictionary";
-import TranslationImpl from "@wayward/game/language/impl/TranslationImpl";
+import type Dictionary from "@wayward/game/language/Dictionary";
+import type TranslationImpl from "@wayward/game/language/impl/TranslationImpl";
 import Translation from "@wayward/game/language/Translation";
 import Mod from "@wayward/game/mod/Mod";
 import Register from "@wayward/game/mod/ModRegistry";
 import { CheckButton } from "@wayward/game/ui/component/CheckButton";
-import Component from "@wayward/game/ui/component/Component";
+import type Component from "@wayward/game/ui/component/Component";
 import { RangeRow } from "@wayward/game/ui/component/RangeRow";
 import { Bound } from "@wayward/utilities/Decorators";
 import { sleep } from "@wayward/utilities/promise/Async";
@@ -135,40 +135,48 @@ export default class ChallengeServer extends Mod {
 		new CheckButton()
 			.setText(translation(ChallengeServerTranslation.OptionLastSurvivingPlayerWins))
 			.setRefreshMethod(() => this.data.lastSurvivingPlayerWinsAutomatically)
-			.event.subscribe("toggle", (_, checked) => { this.data.lastSurvivingPlayerWinsAutomatically = checked; })
+			.event.subscribe("toggle", (_, checked) => {
+				this.data.lastSurvivingPlayerWinsAutomatically = checked;
+			})
 			.appendTo(section);
 	}
 
 	@EventHandler(EventBus.Game, "play")
 	public onGameStart(game: Game, isLoadingSave: boolean, playedCount: number): void {
-		if (game.getGameMode() !== GameMode.Challenge) return;
+		if (game.getGameMode() !== GameMode.Challenge) {
+			return;
+		}
 
 		game.setPaused(true, PauseSource.Generic);
 		this.contenders = new Set();
 		this.winnerName = undefined;
-		sleep(seconds(1)).then(this.waitForPlayers);
+		void sleep(seconds(1)).then(this.waitForPlayers);
 	}
 
 	@EventHandler(EventBus.PlayerManager, "join")
 	public onPlayerJoin(manager: PlayerManager, player: Player): void {
-		if (game.getGameMode() !== GameMode.Challenge) return;
+		if (game.getGameMode() !== GameMode.Challenge) {
+			return;
+		}
 
 		if (this.gameState === GameState.WaitingForPlayers || this.gameState === GameState.Countdown) {
 			this.contenders.add(player.identifier);
 			if (this.gameState === GameState.WaitingForPlayers && game.playerManager.getAll(true, true).length >= this.data.playersToWaitFor) {
-				this.startCountdown();
+				void this.startCountdown();
 			}
 		}
 
 		if (this.gameState === GameState.Ending && !this.winnerName && player.state === PlayerState.None && this.contenders.has(player.identifier)) {
 			// someone that left earlier rejoined, we continue the game for that person
-			this.continueGame();
+			void this.continueGame();
 		}
 	}
 
 	@EventHandler(EventBus.PlayerManager, "leave")
 	public onPlayerLeave(manager: PlayerManager, player: Player): void {
-		if (game.getGameMode() !== GameMode.Challenge) return;
+		if (game.getGameMode() !== GameMode.Challenge) {
+			return;
+		}
 
 		if (this.gameState === GameState.WaitingForPlayers) {
 			this.contenders.delete(player.identifier);
@@ -183,40 +191,45 @@ export default class ChallengeServer extends Mod {
 
 			if (!allPlayers.some(p => p !== player && p.state === PlayerState.None)) {
 				// this was the last living player
-				this.startEndingCountdown();
+				void this.startEndingCountdown();
 			}
 		}
 	}
 
 	@EventHandler(EventBus.Players, "die")
 	public onPlayerDeath(player: Player): void {
-		if (game.getGameMode() !== GameMode.Challenge)
+		if (game.getGameMode() !== GameMode.Challenge) {
 			return;
+		}
 
 		const remainingPlayers = game.playerManager.getAll(true, true).filter(p => p !== player && p.state === PlayerState.None);
-		if (remainingPlayers.length > 1)
+		if (remainingPlayers.length > 1) {
 			return;
+		}
 
 		if (remainingPlayers.length === 1) {
-			if (!this.data.lastSurvivingPlayerWinsAutomatically)
+			if (!this.data.lastSurvivingPlayerWinsAutomatically) {
 				return;
+			}
 
 			this.winnerName = remainingPlayers[0].getName().getString();
-			multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.WinByDefault)
+			void multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.WinByDefault)
 				.addArgs(this.winnerName)
 				.getString());
 		}
 
-		this.startEndingCountdown();
+		void this.startEndingCountdown();
 		return;
 	}
 
 	@EventHandler(EventBus.Players, "sailToCivilization")
 	public onSailToCivilization(player: Player): void {
-		if (game.getGameMode() !== GameMode.Challenge) return;
+		if (game.getGameMode() !== GameMode.Challenge) {
+			return;
+		}
 
 		this.winnerName = player.getName().getString();
-		this.startEndingCountdown();
+		void this.startEndingCountdown();
 	}
 
 	public override onUnload(): void {
@@ -243,7 +256,10 @@ export default class ChallengeServer extends Mod {
 		for (let elapsed = 0; elapsed < countdownTime; elapsed += seconds(1)) {
 			await sleep(seconds(1));
 
-			if (this.gameState !== GameState.Countdown) return;
+			if (this.gameState !== GameState.Countdown) {
+				return;
+			}
+
 			if (!game.isPaused) {
 				this.startGame();
 				return;
@@ -256,12 +272,16 @@ export default class ChallengeServer extends Mod {
 			}
 
 			if (countdownTime - elapsed > minutes(1)) {
-				if (elapsed % seconds(30)) continue; // only log every 30 seconds
+				if (elapsed % seconds(30)) {
+					continue;
+				} // only log every 30 seconds
 			} else if (countdownTime - elapsed > seconds(10)) {
-				if (elapsed % seconds(10)) continue; // only log every 10 seconds
+				if (elapsed % seconds(10)) {
+					continue;
+				} // only log every 10 seconds
 			}
 
-			multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.Countdown)
+			void multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.Countdown)
 				.addArgs(translateTime(countdownTime - elapsed))
 				.getString());
 		}
@@ -271,11 +291,11 @@ export default class ChallengeServer extends Mod {
 
 	private startGame(): void {
 		game.setPaused(false, PauseSource.Generic);
-		multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.Start).getString());
+		void multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.Start).getString());
 		multiplayer.updateOptions({ newPlayerState: PlayerState.Ghost });
 
 		this.elapsed = 0;
-		this.continueGame();
+		void this.continueGame();
 	}
 
 	private async continueGame(): Promise<void> {
@@ -294,7 +314,9 @@ export default class ChallengeServer extends Mod {
 	}
 
 	private async startEndingCountdown(): Promise<void> {
-		if (this.gameState !== GameState.Playing) return;
+		if (this.gameState !== GameState.Playing) {
+			return;
+		}
 
 		this.gameState = GameState.Ending;
 
@@ -303,7 +325,9 @@ export default class ChallengeServer extends Mod {
 		for (let elapsed = 0; elapsed < countdownTime; elapsed += seconds(1)) {
 			await sleep(seconds(1));
 
-			if (this.gameState !== GameState.Ending) return;
+			if (this.gameState !== GameState.Ending) {
+				return;
+			}
 
 			if (elapsed % seconds(5) === 0) {
 				// update description every 5 seconds
@@ -312,17 +336,21 @@ export default class ChallengeServer extends Mod {
 			}
 
 			if (countdownTime - elapsed > seconds(30)) {
-				if (elapsed % seconds(30)) continue; // only log every 30 seconds
+				if (elapsed % seconds(30)) {
+					continue;
+				} // only log every 30 seconds
 			}
 
-			if (elapsed % seconds(10)) continue; // only log every 10 seconds
+			if (elapsed % seconds(10)) {
+				continue;
+			} // only log every 10 seconds
 
-			multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.EndingCountdown)
+			void multiplayer.sendChatMessage(localPlayer, translation(ChallengeServerTranslation.EndingCountdown)
 				.addArgs(translateTime(countdownTime - elapsed))
 				.getString());
 		}
 
-		this.end();
+		void this.end();
 	}
 
 	private async end(): Promise<void> {
